1484da7fd0a2bdcb2b96aa9d6699c9e1236e8117
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-11-21 13:04:42 +0200

    adding search button disabled state

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,40 +5,45 @@ const Searchbar = ({ onSubmit }) => {
   const handlerSubmit = (values, { resetForm }) => {
     // console.log('values :>> ', values);
     //   console.log('props :>> ', onSubmit);
+
     onSubmit(values);
     resetForm();
   };
 
-  const emptySearchString = values => {
-    console.log(values);
+  const searchStringIsEmpty = (values, _) => {
+    console.log('values :>> ', values);
   };
 
   return (
     <header className="searchbar">
-      <Formik
-        initialValues={{ searchingThing: '' }}
-        onSubmit={handlerSubmit}
-        values={emptySearchString}
-      >
-        <Form className="form">
-          <button
-            type="submit"
-            className="button"
-            area-label="Search button"
-            // disabled={values => values.searchingThing === ''}
-          >
-            <span className="button-label">Search</span>
-          </button>
+      <Formik initialValues={{ searchingThing: '' }} onSubmit={handlerSubmit}>
+        {/* Parttern  "Render props" */}
+        {props => {
+          return (
+            <Form className="form">
+              <button
+                type="submit"
+                className="button"
+                area-label="Search button"
+                disabled={
+                  props.values.searchingThing === '' || props.isSubmitting
+                }
+                onClick={searchStringIsEmpty}
+              >
+                <span className="button-label">Search</span>
+              </button>
 
-          <Field
-            className="input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="searchingThing"
-          />
-        </Form>
+              <Field
+                className="input"
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+                name="searchingThing"
+              />
+            </Form>
+          );
+        }}
       </Formik>
     </header>
   );
