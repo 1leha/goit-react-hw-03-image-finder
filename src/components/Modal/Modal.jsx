5ff40dd7e402dd16456dba07aae3f555e13a48f4
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-11-23 12:05:09 +0200

    adding modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 //TODO
 const Modal = ({ imageUrl, tags }) => {
-  return (
+  const modalRoot = document.getElementById('modal-root');
+
+  return createPortal(
     <div className="overlay">
       <div className="modal">
         <img src={imageUrl} alt={tags} />
       </div>
-    </div>
+    </div>,
+    modalRoot
   );
 };
 
